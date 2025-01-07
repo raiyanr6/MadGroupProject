cfd2008a8b4c980afdc18f0289d87c35a9cb15a0
Rafid030 <rafid.rashid30@gmail.com>
2025-01-07 22:46:49 +0800

    Add files via upload

---
+++ NotificationActivity.java
@@ -0,0 +1,28 @@
+package com.example.notification2;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        textView = findViewById(R.id.textViewData);
+        String data = getIntent().getStringExtra("data");
+        textView.setText(data);
+
+
+
+
+    }
+}
